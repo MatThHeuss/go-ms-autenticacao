b5290503dc5f99fce76f9d48e20e2dc8b9566fb1
MatThHeuss <matheusalencar99@gmail.com>
2023-07-11 21:13:29 -0300

    get expin

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"fmt"
 	"github.com/MatThHeuss/go-user-microservice/internal/domain"
 	"github.com/golang-jwt/jwt/v5"
 	"go.uber.org/zap"
@@ -9,15 +10,16 @@ import (
 
 func CreateToken(user *domain.User) string {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"iss":       "auth-server",
-		"expiresAt": time.Second * 1000,
-		"sub":       user.ID,
-		"email":     user.Email,
-		"role":      user.Role,
-		"name":      user.Name,
+		"iss":   "auth-server",
+		"sub":   user.ID,
+		"email": user.Email,
+		"role":  user.Role,
+		"name":  user.Name,
+		"exp":   time.Second * 1000,
 	})
 
 	tokenString, err := token.SignedString([]byte("my_secret_key"))
+	fmt.Println(tokenString)
 	if err != nil {
 		zap.Error(err)
 		return err.Error()
